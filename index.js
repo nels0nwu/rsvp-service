c2b6694697742d6264a485e695eb3880c533ec30
Nelson Wu <nelson@nelsonwu.net>
2020-07-20 15:39:34 +1000

    mongooo

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 const express = require("express");
+const cors = require("cors");
+const monk = require("monk");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(cors());
+app.use(express.json());
+
+const url = "localhost:27017/helloworld";
+const db = monk(url);
+db.then(() => {
+  console.log("Connected correctly to server");
+});
+const messages = db.get("messages");
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello world 😆",
+  });
+});
+
+app.get("/messages", (req, res) => {
+  messages.find().then((messages) => res.json(messages));
+});
+
+function isValidMessage(message) {
+  return (
+    message.name &&
+    message.name.toString().trim() !== "" &&
+    message.content &&
+    message.content.toString().trim() !== ""
+  );
+}
+
+app.post("/messages", (req, res) => {
+  if (isValidMessage(req.body)) {
+    const message = {
+      name: req.body.name.toString(),
+      content: req.body.content.toString(),
+      created: new Date(),
+    };
+    console.log(req.body);
+    messages.insert(message).then((createdMessage) => res.json(createdMessage));
+  } else {
+    res.status(422);
+    res.json({
+      message: "Name and content are required",
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
