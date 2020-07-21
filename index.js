8fb6a04c8968c2680a0df0cdb78187ffed1ce5e2
Nelson Wu <nelson@nelsonwu.net>
2020-07-21 20:27:30 +1000

    use correct port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const monk = require("monk");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
